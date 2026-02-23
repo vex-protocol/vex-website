b870e7ce4df89741bfbc0f9b46aaa3a910452bc3
YukiXBT <extrahash@protonmail.com>
2026-02-22 17:25:50 -0700

    modify scroll

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { useRespawn } from "../context/RespawnContext";
+import { useIsMobile } from "../hooks/useIsMobile";
 
 type Props = {
     sectionIds: readonly string[];
@@ -9,8 +10,9 @@ type Props = {
 
 export function ScrollToTopButton({ sectionIds }: Props): JSX.Element | null {
     const { scrollToTop } = useRespawn();
+    const isMobile = useIsMobile();
 
-    if (sectionIds.length === 0) return null;
+    if (sectionIds.length === 0 || isMobile) return null;
 
     const isMac =
         typeof navigator !== "undefined" &&
